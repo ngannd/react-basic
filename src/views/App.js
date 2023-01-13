39e705e50edfb0bd8380a0a07b7fab547a8d61f7
ngannd <ngannd@esvn.com.vn>
2023-01-13 09:21:50 +0700

    cmt

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 123
+          Hello World 123 456
         </p>
         <a
           className="App-link"
